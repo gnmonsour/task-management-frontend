d529e73000d69d6946ab74e6863127f32211d87f
George N Monsour <gnmonsour@gmail.com>
2020-03-01 14:24:44 -0500

    adjustments to interoperate locally

--- src/services/base-http.service.js
+++ src/services/base-http.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default class BaseHttpService {
-  BASE_URL = 'http://localhost:3000';
+  BASE_URL = 'http://localhost:3030';
   _accessToken = null;
 
   constructor(routerStore) {
